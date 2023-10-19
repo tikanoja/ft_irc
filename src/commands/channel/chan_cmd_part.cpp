34b626e1dec5ada80ac9df1d591d92a54e8a5160
Tuukka Tikanoja <ttikanoj@c2r5p1.hive.fi>
2023-10-19 11:19:48 +0300

    part works with a list of channels

--- src/commands/channel/chan_cmd_part.cpp
+++ src/commands/channel/chan_cmd_part.cpp
@@ -6,7 +6,7 @@
 /*   By: ttikanoj <ttikanoj@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 09:41:01 by djagusch          #+#    #+#             */
-/*   Updated: 2023/10/18 14:42:29 by ttikanoj         ###   ########.fr       */
+/*   Updated: 2023/10/19 11:18:08 by ttikanoj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,25 +28,35 @@ int chan_cmd_part(IRCServer& server, User& user, Message& message){
 		user.send(ERR_NEEDMOREPARAMS(server.getName(), "PART"));
 		return 1;
 	}
-	Channel* partFrom = server.getChannels().findChannel(*message.getParams().begin());
-	if (partFrom == NULL) {
-		user.send(ERR_NOSUCHCHANNEL(server.getName(), message.getParams().front()));
-		return 1;
-	}
-	//check if user is present in the channel
-	for (std::vector<User*>::iterator it = partFrom->getMembers()->begin();\
-	it != partFrom->getMembers()->end(); it++) {
-		if ((*it)->getNick() == user.getNick()) {
-			partFrom->getMembers()->erase(it);
-			user.send(":" + user.getNick() + "!add_user_host_here" + " PART " + partFrom->getName());
-			if (message.getTrailing() != "")
-				user.send(" :" + message.getTrailing() + "\r\n"); //add userhost? add trailing message? check out log from dalnet
-			else
-				user.send("\r\n");
-			return 0;
+	
+	std::stringstream ss(message.getParams().front());
+	std::string chan;
+	int user_found = 0;
+	while (std::getline(ss, chan, ',')) {
+		Channel* partFrom = server.getChannels().findChannel(chan);
+		if (partFrom == NULL) {
+			user.send(ERR_NOSUCHCHANNEL(server.getName(), chan));
+			continue ;
 		}
+		//check if user is present in the channel
+		for (std::vector<User*>::iterator it = partFrom->getMembers()->begin();\
+		it != partFrom->getMembers()->end(); it++) {
+			if ((*it)->getNick() == user.getNick()) {
+				partFrom->getMembers()->erase(it);
+				user.send(":" + user.getNick() + "!add_user_host_here" + " PART " + partFrom->getName());
+				std::cout << "HERE!!!!" << std::endl;
+				if (message.getTrailing() != "")
+					user.send(" :" + message.getTrailing() + "\r\n"); //add userhost? add trailing message? check out log from dalnet
+				else
+					user.send("\r\n");
+				user_found = 1;
+				break ;
+			}
+		}
+		if (user_found == 0)
+			user.send(ERR_NOTONCHANNEL(server.getName(), partFrom->getName()));
+		user_found = 0;
 	}
-	user.send(ERR_NOTONCHANNEL(server.getName(), partFrom->getName()));
 	//if yes then get rid of them
 	return 1;
 }
