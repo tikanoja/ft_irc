630a0080b5431cb308d1e6af76370d777b084c64
djagusch <djagusch@student.hive.figit config --global user.name Dorian>
2023-11-02 17:18:55 +0200

    fixed buffer segfault

--- src/IRCServer/IRCServer.cpp
+++ src/IRCServer/IRCServer.cpp
@@ -6,7 +6,7 @@
 /*   By: djagusch <djagusch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 23:21:45 by tuukka            #+#    #+#             */
-/*   Updated: 2023/11/02 17:11:53 by djagusch         ###   ########.fr       */
+/*   Updated: 2023/11/02 17:15:53 by djagusch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -138,28 +138,23 @@ int IRCServer::pollingRoutine() {
 	p_fd_count = static_cast<nfds_t>(p_pfds.size());
 	signal(SIGINT, signalHandler);
 	while (1) {
-		if ((poll_count = poll(&(p_pfds[0]), p_fd_count, 300)) == -1)
+		if ((poll_count = poll(&(p_pfds[0]), p_fd_count, 0)) == -1)
 			return (-1);
 		for (nfds_t i = 0; i < p_fd_count; i++) {
 			if (p_pfds[i].revents & (POLLIN | POLLOUT | POLLNVAL | POLLERR)) {
 				if (i == 0) {
 					if (acceptClient()){
 						p_logger->log("Failed to accept new client", __FILE__, __LINE__);
-						// continue ;
 					}
 				} else if (p_pfds[i].revents & POLLIN) {
 					receiveMsg(p_users.findUserBySocket(p_pfds[i].fd), i);
- 					// if (receiveMsg(p_users.findUserBySocket(p_pfds[i].fd), i))
-					// 	continue ;
 				} else if (p_pfds[i].revents & POLLOUT) {
-					// std::cout << "POLLOUT TRIGGER!!!" << std::endl;
-					checkRecvBuffer(p_users.findUserBySocket(p_pfds[i].fd), i);
 					checkSendBuffer(p_users.findUserBySocket(p_pfds[i].fd));
+					checkRecvBuffer(p_users.findUserBySocket(p_pfds[i].fd), i);
 				}
 				else {
 					dropConnection(-1, i);
 					p_logger->log("Connection dropped", __FILE__, __LINE__);
-					// continue;
 				}
 			}
 		}
