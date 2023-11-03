b10677be79da65c83245513747083780d4cfbecf
tikanoja <tuukka.tikanoja@gmail.com>
2023-11-03 16:46:44 +0200

    FINISHED?

--- src/main.cpp
+++ src/main.cpp
@@ -3,36 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: djagusch <djagusch@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ttikanoj <ttikanoj@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 22:45:35 by tuukka            #+#    #+#             */
-/*   Updated: 2023/11/01 16:03:16 by djagusch         ###   ########.fr       */
+/*   Updated: 2023/11/03 16:45:59 by ttikanoj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/IRCServer.hpp"
 
 int main(int ac, char** av) {
-	
-	if (ac != 3) {
-		std::cerr << "usage: " << av[0] << " <port> <password>" << std::endl;
-		return (1);
-	}
-	
-	int portInt = 0;
-	std::istringstream(av[1]) >> portInt;
-	if (portInt <= 0 || portInt > 65535) {
-		std::cerr << "bad port number: " << av[1] << std::endl;
-		return (1);
-	}
-	uint16_t port = static_cast<uint16_t>(portInt);
-	std::string password(av[2]);
 
 	try {
+		if (ac != 3) {
+			std::cerr << "usage: " << av[0] << " <port> <password>" << std::endl;
+			return (1);
+		}
+		
+		int portInt = 0;
+		std::istringstream(av[1]) >> portInt;
+		if (portInt <= 0 || portInt > 65535) {
+			std::cerr << "bad port number: " << av[1] << std::endl;
+			return (1);
+		}
+		uint16_t port = static_cast<uint16_t>(portInt);
+		std::string password(av[2]);
+
 		IRCServer server(port, password);
 		if (server.pollingRoutine())
 			throw std::runtime_error("Polling failed");
-	} catch (std::exception& e) {
+	}
+	catch (std::exception& e) {
+		std::cerr << "Server quit: " << e.what() << std::endl;
+	}
+	catch (std::bad_alloc& e) {
 		std::cerr << "Server quit: " << e.what() << std::endl;
 	}
 	return (0);
