760126f3464da0f180a1a9f87d0df29758e86e34
djagusch <djagusch@student.hive.figit config --global user.name Dorian>
2023-11-07 08:56:34 +0200

    finished??

---
+++ src/commands/cmd_who.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cmd_who.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: djagusch <djagusch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/04 09:43:33 by djagusch          #+#    #+#             */
+/*   Updated: 2023/11/07 08:12:58 by djagusch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/Commands.hpp"
+#include "../inc/Utils.hpp"
+
+int cmd_who(IRCServer& server, User& user, Message& message){
+	
+	if (!(user.getMode() & IRCServer::registered)){
+		user.send(ERR_NOTREGISTERED(server.getName(),
+		message.getCommand()));
+		return 1;
+	}
+	if (message.getParams().size() > 1)
+		user.send(RPL_ENDOFWHO(server.getName(), user.getNick(), message.getParams()[1]));
+	else
+		user.send(RPL_ENDOFWHO2(server.getName(), user.getNick()));
+	return 0;
+}
+
+int cmd_whois(IRCServer& server, User& user, Message& message){
+	
+	if (!(user.getMode() & IRCServer::registered)){
+		user.send(ERR_NOTREGISTERED(server.getName(),
+		message.getCommand()));
+		return 1;
+	}
+	user.send(RPL_ENDOFWHOIS(server.getName(), user.getNick()));
+	return 0;
+}
